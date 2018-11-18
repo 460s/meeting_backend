5d4a61b50648a25d334b263817b1f8dc025e750a
Konstantin Slastnoy <sikroz@ispsystem.com>
2018-11-18 16:54:58 +0800

    format

--- main.cpp
+++ main.cpp
@@ -1,17 +1,17 @@
-#include <iostream>
 #include <cstdlib>
+#include <iostream>
 
 using namespace std;
 
 namespace ispsystem::hello {
-    string GetString () {
-        auto user = getenv("USERNAME");
-        return user == nullptr ? "World" : user;
-    }
-
+string GetString() {
+	auto user = getenv("USERNAME");
+	return user == nullptr ? "World" : user;
 }
 
+} // namespace ispsystem::hello
+
 int main() {
-    cout << "Hello " << ispsystem::hello::GetString() << endl;
-    return 0;
+	cout << "Hello " << ispsystem::hello::GetString() << endl;
+	return 0;
 }
