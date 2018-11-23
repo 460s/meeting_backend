3b35f0f3c0149e58a60a3779091f03d98ff509db
The Gull <zxcvb.tatuyc@yandex.ru>
2018-11-23 15:18:26 +0800

    Homework1, Tatuyc

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+#include <Poco/URI.h>
+#include <nlohmann/json.hpp>
 
 using namespace std;
 
 int main() {
+	string uri_string;
+	cin >> uri_string;
+	nlohmann::json result;
+	Poco::URI uri1(uri_string);
+	result["schema"] = uri1.getScheme();
+	result["host"] = uri1.getHost();
+	result["path"] = uri1.getPath();
+	cout << result.dump(1) << endl;
 	return 0;
 }
