243081e1a4cdd2b444917a077a1908d55a6ade3f
Kotoff <chekoopa@mail.ru>
2018-11-23 13:04:27 +0800

    it works!

--- main.cpp
+++ main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
 
+#include <Poco/URI.h>
+#include <nlohmann/json.hpp>
+
 using namespace std;
 
 int main() {
+	string uri_string;
+	cin >> uri_string;
+	
+	Poco::URI uri(uri_string);
+	nlohmann::json result;
+	
+	result["schema"] = uri.getScheme();
+	result["host"] = uri.getHost();
+	result["path"] = uri.getPath();
+
+	cout << result << endl;
+
 	return 0;
 }
+
