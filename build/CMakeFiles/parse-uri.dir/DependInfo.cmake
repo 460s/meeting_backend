9a78e72843dc7fd8189940654a02a281ecd59e8f
m.kovalenko <job1123@list.ru>
2018-11-26 04:32:37 +0800

    parse uri

---
+++ build/CMakeFiles/parse-uri.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/maxim/meeting_backend/main.cpp" "/home/maxim/meeting_backend/build/CMakeFiles/parse-uri.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/maxim/.conan/data/Poco/1.9.0/pocoproject/stable/package/f3edb316e670b1a645e5f3540fbb428ac7831c87/include"
+  "/home/maxim/.conan/data/jsonformoderncpp/3.3.0/vthiery/stable/package/5ab84d6acfe1f23c4fae0ab88f26e3a396351ac9/include"
+  "/home/maxim/.conan/data/OpenSSL/1.0.2o/conan/stable/package/567d3c8a946fef28f3d33af82c9b7a20c8fe87ae/include"
+  "/home/maxim/.conan/data/zlib/1.2.11/conan/stable/package/69992a588026a3e14cd49c40ed17369c46d8d3a5/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+#include <Poco/URI.h>
+#include <nlohmann/json.hpp>
 
 using namespace std;
 
 int main() {
+	std::string string_uri;
+	std::cin >> string_uri;
+	Poco::URI uri1(string_uri);
+	nlohmann::json result;
+	result["schema"]=uri1.getScheme();
+	result["host"]=uri1.getHost();
+	result["path"]=uri1.getPath();
+	std::cout << result << std::endl;
 	return 0;
 }
