d68af2543e35110892e820a1ea6e102b553cf967
Niss <email>
2018-11-25 20:47:27 +0800

    Ready homework

--- main.cpp
+++ main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
+#include <Poco/URI.h>
+#include <nlohmann/json.hpp>
 
 using namespace std;
 
 int main() {
+	string str;
+	cout << "Enter uri string: ";
+	cin >> str;
+
+	Poco::URI uri(str);
+
+	nlohmann::json json_struct;
+
+	json_struct["scheme"] = uri.getScheme();
+	json_struct["host"] = uri.getHost();
+	json_struct["path"] = uri.getPath();
+
+	cout << json_struct << endl;
+	
 	return 0;
 }
