5c8099a6593cd678383f128c44524908953ef797
k-i-a <ivan.kudritskiy@gmail.com>
2018-11-25 21:35:07 +0800

    homework-1 done

--- main.cpp
+++ main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 
+#include <Poco/URI.h>
+#include <nlohmann/json.hpp>
+
 using namespace std;
 
 int main() {
+	
+	string inputStr;
+	cin >> inputStr;
+	Poco::URI uri(inputStr);
+	nlohmann::json outputJSON = {
+		{"scheme", uri.getScheme()},
+		{"host", uri.getHost()},
+		{"path", uri.getPath()},
+	};
+
+	cout << outputJSON.dump() << endl;
 	return 0;
 }
