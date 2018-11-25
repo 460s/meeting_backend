8dfd82dedc0c3fb0493cdafc1af7b760304b32ad
Alexandr Vetrov <a.vetrov@ispsystem.com>
2018-11-26 00:01:25 +0800

    Poco/URI.h

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
+#include "Poco/URI.h"
 #include <iostream>
 
 using namespace std;
 
-int main() {
-	return 0;
+int main(int argc, char** argv) {
+	std::string string_uri = "https://github.com/460s/meeting_backend/edit/hello-conan/README.md";
+	// cin >> string_uri;
+	Poco::URI uri(string_uri);
+	std::string scheme(uri.getScheme()); // "http"
+	std::string host(uri.getHost()); // "www.appinf.com"
+	std::string path(uri.getPath()); // "/sample"
+	cout << scheme << endl << host << endl << path;
+	return 0;	
 }
