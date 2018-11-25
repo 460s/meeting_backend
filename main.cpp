204a1631520627428a244ae65c4ba708aa13c4a4
Vitvickii Dmitrii <vitvickiid1@mail.ru>
2018-11-25 18:53:37 +0800

    Парсинг URI

--- main.cpp
+++ main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
-
-using namespace std;
+#include <Poco/URI.h>
+#include <nlohmann/json.hpp>
 
 int main() {
+	std::string uri_str;
+	std::cin >> uri_str;
+
+	Poco::URI uri(uri_str);
+	nlohmann::json result;
+
+	result["schema"] = uri.getScheme();
+	result["host"] = uri.getHost();
+	result["path"] = uri.getPath();
+	
+	std::cout << result << std::endl;
+
 	return 0;
 }
