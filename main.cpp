1a3efa94e6941e9525974b8d842cff2881ea85eb
JimBeam38 <4312786590435@mail.ru>
2018-11-25 22:15:27 +0800

    homework-1

--- main.cpp
+++ main.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
+#include <Poco/URI.h>
+#include <nlohmann/json.hpp>
 
 using namespace std;
 
-int main() {
+int main(int argc, char** argv)
+{
+	Poco::URI uri1("https://github.com/460s/meeting_backend/edit/hello-conan/README.md");
+	std::string scheme(uri1.getScheme());
+	std::string host(uri1.getHost()); 
+	std::string path(uri1.getPath());
+
+	nlohmann::json schemeOut;
+	schemeOut["scheme"] = scheme;
+	std::cout << schemeOut << std::endl;
+
+	nlohmann::json hostOut;
+	hostOut["host"] = host;
+	std::cout << hostOut << std::endl;
+
+	nlohmann::json pathOut;
+	pathOut["path"] = path;
+	std::cout << pathOut << std::endl;
+
 	return 0;
+
 }
