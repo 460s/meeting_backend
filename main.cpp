603ffe792d0df24967b3f0778bf3ae74b1d78845
nerd <nerd0_0@hotmail.com>
2018-11-26 16:44:20 +0800

    done

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+#include <nlohmann/json.hpp>
+#include <Poco/URI.h>
 
 using namespace std;
 
 int main() {
+    string s;
+    cin >> s;
+    Poco::URI uri1(s);
+    nlohmann::json j;
+    j["schema"]=uri1.getScheme();
+    j["host"]=uri1.getHost();
+    j["path"]=uri1.getPath();
+    cout << j.dump() << endl;
 	return 0;
 }
