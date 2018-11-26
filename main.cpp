9f0ce71470f29f71ac981677a3eb303c6c47ade2
Shurik <avkaaa@mail.ru>
2018-11-26 18:27:11 +0800

    Homework1

--- main.cpp
+++ main.cpp
@@ -4,13 +4,15 @@
 #include <nlohmann/json.hpp>
 
 int main() {
-std::string uri_str;
-std::cin >> uri_str;
-Poco::URI uri(uri_str);
-nlohmann::json result;
-result["schema"] = uri.getScheme();
-result["host"] = uri.getHost();
-result["path"] = uri.getPath();
-std::cout << result << std::endl;
-return 0;
+	std::string uri_str;
+	std::cin >> uri_str;
+	Poco::URI uri(uri_str);
+	
+	nlohmann::json result;
+	result["schema"] = uri.getScheme();
+	result["host"] = uri.getHost();
+	result["path"] = uri.getPath();
+	std::cout << result << std::endl;
+	
+	return 0;
 }
