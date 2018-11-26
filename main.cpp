bc998636f9e1f61523752b6b81deb7d460264166
Sorokovikov <themilkymouse@gmail.com>
2018-11-26 15:34:13 +0800

    Сороковиков Р.Н., ДЗ-1

--- main.cpp
+++ main.cpp
@@ -1,7 +1,39 @@
+#include "Poco/URI.h"
+#include <nlohmann/json.hpp>
+
 #include <iostream>
 
 using namespace std;
+using json = nlohmann::json;
 
-int main() {
+
+string ifStringEmpty(string s)
+{
+	if (s == "")
+	{
+		return "none";
+	}
+	return s;
+}
+
+int main()
+{
+	string text = "";
+	cin >> text;
+	Poco::URI uri(text);
+
+	string scheme(ifStringEmpty(uri.getScheme()));
+	string host(ifStringEmpty(uri.getHost()));
+	string path(ifStringEmpty(uri.getPath()));
+
+	json jsonResult =
+	{
+		{"scheme", scheme},
+		{"host", host},
+		{"path", path}
+	};
+
+	cout << jsonResult << endl;
+
 	return 0;
 }
