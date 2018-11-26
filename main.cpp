a6980d1c86a34ae8aa6e61c47c9482d16a433176
Denis Naumov <naumov.da@underme.ru>
2018-11-26 10:08:33 +0800

    Implemented URI parser

--- main.cpp
+++ main.cpp
@@ -1,7 +1,19 @@
-#include <iostream>
 
-using namespace std;
+#include <iostream>
+#include <Poco/URI.h>
+#include <nlohmann/json.hpp>
 
 int main() {
+
+	std::string uriString;
+	std::cin >> uriString;
+	Poco::URI uri(uriString);
+	nlohmann::json result;
+
+	result["schema"] = uri.getScheme();
+	result["host"] = uri.getHost();
+	result["path"] = uri.getPath();
+
+	std::cout << result; 
 	return 0;
 }
