0196cfea2fa2a72eeff0161d4fbeae69f5810e36
AOseledec <artemij.oseledets@yandex.ru>
2018-11-27 21:38:33 +0800

    ДЗ-1

--- main.cpp
+++ main.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
+#include <Poco/URI.h>
+#include <nlohmann/json.hpp>
 
 using namespace std;
 
 int main() {
-	return 0;
+	        //
+        std::string uri_string;
+        std::cin >> uri_string;
+
+        //
+        Poco:: URI uri1(uri_string);
+
+        //
+        std::string scheme(uri1.getScheme());
+        std::string host(uri1.getHost());
+        std::string path(uri1.getPath());
+
+        //
+        nlohmann::json result;
+
+        //
+        result["scheme"] = scheme;
+        result["host"]   = host;
+        result["path"]   = path;
+
+        //
+        std::cout << result << std::endl;
+        return 0;
+
 }
