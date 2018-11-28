52b2abc0e5407c40fae9667b2b6108f915d1edb1
nerd <nerd0_0@hotmail.com>
2018-11-28 14:45:00 +0800

    get с несуществующим индексом не роняет сервер

--- handlers/src/user_meeting.cpp
+++ handlers/src/user_meeting.cpp
@@ -40,6 +40,7 @@ public:
 	virtual int Delete(int id) = 0;
 	virtual ~Storage() {}
 	virtual Meeting Get(int ident) = 0;
+	virtual int size() = 0;
 };
 
 class MapStorage : public Storage {
@@ -72,13 +73,13 @@ public:
 		} else
 			return 1;
 	}
-	Meeting Get(int ident) {
-		if (ident < m_meetings.size()) {
-			for (auto [id, meeting] : m_meetings) {
-				if(id==ident) return meeting;
-			}
-
+	Meeting Get(int ident) override {
+		for (auto [id, meeting] : m_meetings) {
+			if (id == ident) return meeting;
 		}
+	}
+	int size() override {
+		return m_meetings.size();
 	}
 
 private:
@@ -140,11 +141,13 @@ void UserMeetingDelete::handleRequest(Poco::Net::HTTPServerRequest &request, Poc
 
 void UserMeetingGet::handleRequest(Poco::Net::HTTPServerRequest &request, Poco::Net::HTTPServerResponse &response) {
 	response.setStatus(Poco::Net::HTTPServerResponse::HTTP_OK);
-	auto &storage=GetStorage();
-	int id=std::stoi(request.getURI().substr(14));
-	Meeting resp=storage.Get(id);
-	if(resp.id==id) response.send() << json(resp);
-	else response.send() << "Hasn't meeting with id = " << id;
+	auto &storage = GetStorage();
+	int id = std::stoi(request.getURI().substr(14));
+	if (id < storage.size()) {
+		Meeting resp = storage.Get(id);
+		response.send() << json(resp);
+	} else
+		response.send() << "Hasn't meeting with id = " << id;
 }
 
 } // namespace handlers
