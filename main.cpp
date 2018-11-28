091379b53de048048578926b04d3cb873abc2511
AOseledec <artemij.oseledets@yandex.ru>
2018-11-28 12:04:08 +0800

    исправлены замечания

--- main.cpp
+++ main.cpp
@@ -5,27 +5,16 @@
 using namespace std;
 
 int main() {
-	        //
+
         std::string uri_string;
         std::cin >> uri_string;
 
-        //
-        Poco:: URI uri1(uri_string);
+        Poco:: URI poco_uri(uri_string);
 
-        //
-        std::string scheme(uri1.getScheme());
-        std::string host(uri1.getHost());
-        std::string path(uri1.getPath());
+        result["scheme"] = uri1.getScheme();
+        result["host"]   = uri1.getHost();
+        result["path"]   = uri1.getPath();
 
-        //
-        nlohmann::json result;
-
-        //
-        result["scheme"] = scheme;
-        result["host"]   = host;
-        result["path"]   = path;
-
-        //
         std::cout << result << std::endl;
         return 0;
 
