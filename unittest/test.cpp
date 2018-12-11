982c636e2660cba34f3ed71f8ab72326d4b339d5
Vitvickii Dmitrii <vitvickiid1@mail.ru>
2018-12-12 01:37:27 +0800

    Homework 6

--- unittest/test.cpp
+++ unittest/test.cpp
@@ -37,3 +37,44 @@ json target = R"(
 
 CHECK(json::diff(json(source),target).empty() == true);
 }
+
+TEST_CASE("json_unmarshal") {
+	handlers::Meeting source;
+	source.name = "test";
+	source.address = "addr";
+	source.description ="desc";
+	source.signup_description = "sdesc";
+	source.signup_from_date = 1;
+	source.signup_to_date = 2;
+	source.from_date = 3;
+	source.to_date = 4;
+	source.published = true;
+
+	json target = R"(
+		{
+			"name": "test",
+			"address": "addr",
+			"description": "desc",
+			"signup_description": "sdesc",
+			"signup_from_date": 1,
+			"signup_to_date": 2,
+			"from_date": 3,
+			"to_date": 4,
+			"published": true
+		}
+	)"_json;
+
+	handlers::Meeting meeting = handlers::Meeting(target);
+
+	CHECK((
+		meeting.name == source.name &&
+		meeting.address == source.address &&
+		meeting.description == source.description &&
+		meeting.signup_description == source.signup_description &&
+		meeting.signup_from_date == source.signup_from_date &&
+		meeting.signup_to_date == source.signup_to_date &&
+		meeting.from_date == source.from_date &&
+		meeting.to_date == source.to_date &&
+		meeting.published == source.published));
+
+}
